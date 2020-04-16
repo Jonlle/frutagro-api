2f407af68a0360e1897f1bc3031308e9e87d3953
Jonlle <jonlle19@gmail.com>
2020-04-16 00:03:38 -0400

    Modifica User resource

--- app/Http/Resources/User.php
+++ app/Http/Resources/User.php
@@ -17,12 +17,10 @@ class User extends JsonResource
         return [
             'id' => $this->id,
             'username' => $this->username,
-            'doc_type_id' => $this->doc_type_id,
-            'role_id' => $this->role_id,
-            'status_id' => $this->status_id,
-            'first_name' => $this->first_name,
-            'last_name' => $this->last_name,
-            'document' => $this->document,
+            'role' => $this->role_id,
+            'status' => $this->status->description,
+            'name' => $this->name,
+            'email' => $this->email->email,
             'created_at' => $this->created_at,
             'updated_at' => $this->updated_at,
         ];
