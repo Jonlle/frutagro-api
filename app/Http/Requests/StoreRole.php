241a8d37918de3766e1cd4f1d7b3607c39460395
Jonlle <jonlle19@gmail.com>
2020-04-28 23:40:50 -0400

    Crea FormRequests para Role
    - Store
    - Update

---
+++ app/Http/Requests/StoreRole.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Requests;
+
+class StoreRole extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'id' => 'required|unique:roles|max:8',
+            'status_id' => 'required|max:2',
+            'description' => 'required|max:50',
+            'permissions.*' => 'integer',
+            'permissions' => 'required|array'
+        ];
+    }
+}
