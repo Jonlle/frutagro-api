1966237b2e03296ca520b54bc06fa642efc6d8ec
Jonlle <jonlle19@gmail.com>
2020-05-14 00:33:59 -0400

    Crea FormRequests para Category
    - Store
    - Update

---
+++ app/Http/Requests/UpdateCurrencyCode.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Requests;
+use Illuminate\Validation\Rule;
+
+class UpdateCurrencyCode extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'currency_name' => [
+                'required',
+                'max:10',
+                Rule::unique('currency_codes')->ignore($this->route('currency')),
+            ],
+            'exchange_rate' => 'required|numeric'
+        ];
+    }
+}
