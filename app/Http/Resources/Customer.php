8d43a9fe65d2cef18b5a693ffe0f50c0d1f8837c
Jonlle <jonlle19@gmail.com>
2020-05-16 01:42:46 -0400

    Crea Resource y Collection para Customers

---
+++ app/Http/Resources/Customer.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class Customer extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'username' => $this->username,
+            'role' => $this->role_id,
+            'status' => $this->status->description,
+            'name' => $this->name,
+            'email' => $this->user_emails->where('principal', '1')->first()->email,
+            'doc_type' => $this->doc_type_id,
+            'document' => $this->document,
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+        ];
+    }
+}
