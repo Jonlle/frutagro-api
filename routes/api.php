296b4b257a44b6a2c75ffaf542b7c52edb945b74
Jonlle <jonlle19@gmail.com>
2020-05-16 01:40:27 -0400

    Incorpora customers route

--- routes/api.php
+++ routes/api.php
@@ -34,6 +34,7 @@ Route::prefix('v1')->group(function () {
     Route::apiResource('/permissions', 'API\PermissionController');
     Route::apiResource('/roles', 'API\RoleController');
     Route::apiResource('/users', 'API\UserController');
+    Route::apiResource('/customers', 'API\CustomerController');
     Route::apiResource('/categories', 'API\CategoryController');
     Route::apiResource('/currencies', 'API\CurrencyCodeController');
     Route::apiResource('/products', 'API\ProductController');
