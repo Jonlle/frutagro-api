83123694c6024f08d5374008f59cfe9a29f1cab4
Jonlle <jonlle19@gmail.com>
2020-05-19 01:23:55 -0400

    Modifica AuthController

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Requests\StoreUser;
+use App\Http\Requests\StoreCustomer;
 use App\Http\Resources\UserCollection;
 use App\Http\Resources\User as UserResource;
 use App\User;
@@ -23,34 +24,27 @@ class AuthController extends BaseController
      */
     public function __construct()
     {
-        $this->middleware('auth:api')->except('register','login','registerAdmin','loginAdmin');
+        $this->middleware('auth:api')->except('login', 'register', 'registerAdmin');
     }
 
-    public function register(Request $request)
+    public function register(StoreCustomer $request)
     {
-        $validator = Validator::make($request->all(), [
-            'username' => 'required|max:10',
-            'doc_type_id' => 'required|max:3',
-            'role_id' => 'required|max:8',
-            'status_id' => 'required|max:2',
-            'first_name' => 'required',
-            'last_name' => 'required',
-            'document' => 'required',
-            'password' => 'required',
+        $validated = $request->validated();
+
+        $user_email = new UserEmail([
+            'email' => $request['email'],
+            'status_id' => 'a',
+            'principal' => '1'
         ]);
 
-        if($validator->fails()){
-            return $this->sendError('Validation error.', $validator->errors(), BaseController::HTTP_UNPROCESSABLE_ENTITY);
-        }
-
-        if (User::firstWhere('username', $request->username)) {
-            return $this->sendError('A user with this username already exists.', [], BaseController::HTTP_CONFLICT);
-        }
-
-        $data = $request->all();
+        $data = Arr::except($validated, ['email']);
         $data['password'] = bcrypt($data['password']);
-        $user = User::create($data);
-        $success =  $user;
+
+        $user = new User($data);
+        $user->save();
+        $user->user_emails()->save($user_email);
+
+        $success['username'] = $user->username;
 
         return $this->sendResponse($success, 'User register successfully.', BaseController::HTTP_CREATED);
     }
@@ -80,35 +74,7 @@ class AuthController extends BaseController
     public function login(Request $request)
     {
         $validator = Validator::make($request->all(), [
-            'username' => 'required',
-            'password' => 'required',
-        ]);
-
-        if($validator->fails()){
-            return $this->sendError('Validation error.', $validator->errors(), BaseController::HTTP_UNPROCESSABLE_ENTITY);
-
-        }
-
-        $credentials = [
-            'username' => $request->username,
-            'password' => $request->password
-        ];
-
-        if (auth()->attempt($credentials)) {
-        $user = Auth::user();
-        $success['token'] = $user->createToken('Frutagro_dist')->accessToken;
-        $success['username'] =  $user->username;
-
-        return $this->sendResponse($success, 'User login successfully.');
-        } else {
-            return $this->sendError('Unauthorised.', [], BaseController::HTTP_UNAUTHORIZED);
-        }
-    }
-
-    public function loginAdmin(Request $request)
-    {
-        $validator = Validator::make($request->all(), [
-            'username' => 'required',
+            'login' => 'required',
             'password' => 'required',
         ]);
 
@@ -118,33 +84,28 @@ class AuthController extends BaseController
         }
 
         $credentials = ['password' => $request->password];
-        $fieldType = filter_var($request->username, FILTER_VALIDATE_EMAIL) ? 'email' : 'username';
-        Log::debug('fieldType: '.json_encode($fieldType));
+        $fieldType = filter_var($request->login, FILTER_VALIDATE_EMAIL) ? 'email' : 'username';
+
         if ($fieldType == 'email') {
-            $user_email = UserEmail::where('email', $request->username)->first();
-            $credentials['id'] = $user_email->user_id;
+            $user_email = UserEmail::where('email', $request->login)->first();
+            $credentials['username'] = ($user_email) ? $user_email->user->username : '';
         } else {
-            $credentials['username'] =  $request->username;
+            $credentials['username'] =  $request->login;
         }
 
         if (auth()->attempt($credentials)) {
-        $user = Auth::user();
-        $success['token'] = $user->createToken('Frutagro_dist')->accessToken;
-        $success['username'] =  $user->username;
+            $user = Auth::user();
+            $success['token'] = $user->createToken('Frutagro_dist')->accessToken;
+            $success['username'] =  $user->username;
+            $success['role'] =  $user->role_id;
 
-        return $this->sendResponse($success, 'User login successfully.');
+            return $this->sendResponse($success, 'User login successfully.');
         } else {
             return $this->sendError('Unauthorised.', [], BaseController::HTTP_UNAUTHORIZED);
         }
     }
 
     public function logout(Request $request)
-    {
-        $request->user()->token()->revoke();
-        return response()->json(['message' =>'User logout successfully'], 200);
-    }
-
-    public function logoutAdmin(Request $request)
     {
         $request->user()->token()->revoke();
         return response()->json(['message' =>'User logout successfully'], 200);
