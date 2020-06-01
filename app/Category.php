945247fffeb141b1015f3ece13ccfa747b3cf5d5
Jonlle <jonlle19@gmail.com>
2020-05-31 21:47:28 -0400

    Modifica Category Model

--- app/Category.php
+++ app/Category.php
@@ -7,7 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 class Category extends Model
 {
     public $incrementing = false;
-    public $timestamps = false;
 
     protected $fillable = [
         'id', 'slug', 'description'
