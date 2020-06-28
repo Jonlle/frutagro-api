e00b51a958888da88a8995f316b220b4ad56813c
Jonlle <jonlle19@gmail.com>
2020-06-28 19:55:43 -0400

    Modifica UserPhone Model

--- app/UserPhone.php
+++ app/UserPhone.php
@@ -6,5 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class UserPhone extends Model
 {
-    // protected $keyType = 'string';
+    protected $fillable = [
+        'phone_number', 'status_id', 'user_id', 'principal'
+    ];
+
+    public function status()
+    {
+        return $this->belongsTo('App\Status');
+    }
+
+    public function user()
+    {
+        return $this->belongsTo('App\User');
+    }
 }
