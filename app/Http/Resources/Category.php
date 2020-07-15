bb9e19f298a4abab15a9d6218855266254460aef
Jonlle <jonlle19@gmail.com>
2020-07-15 01:29:59 -0400

    Modifica Category Resource

--- app/Http/Resources/Category.php
+++ app/Http/Resources/Category.php
@@ -16,6 +16,7 @@ class Category extends JsonResource
     {
         return [
             'id' => $this->id,
+            'name' => ucfirst($this->id),
             'slug' => $this->slug,
             'description' => $this->description,
             'created_at' => $this->created_at,
