d6edfcc80ba2d5a3a0aaa9291d2561741376f5fe
Jonlle <jonlle19@gmail.com>
2020-07-25 19:14:45 -0400

    Renombra ciudades Migration

--- database/migrations/2020_07_08_230954_create_ciudades_table.php
+++ database/migrations/2020_07_08_230954_create_ciudades_table.php
@@ -4,8 +4,14 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateCiudadsTable extends Migration
+class CreateCiudadesTable extends Migration
 {
+    /**
+     * Schema table name to migrate
+     * @var string
+     */
+    public $tableName = 'ciudades';
+
     /**
      * Run the migrations.
      *
@@ -13,7 +19,7 @@ class CreateCiudadsTable extends Migration
      */
     public function up()
     {
-        Schema::create('ciudades', function (Blueprint $table) {
+        Schema::create($this->tableName, function (Blueprint $table) {
             $table->id();
             $table->foreignId('estado_id')->constrained()
                   ->onDelete('cascade');
