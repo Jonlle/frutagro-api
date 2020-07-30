57e17dfbe878b1d4c52fd5170f744e130578c9fa
Jonlle <jonlle19@gmail.com>
2020-07-29 21:49:34 -0400

    Crea Addrees Resource y Collection

---
+++ app/Http/Resources/Address.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class Address extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'type' => $this->type_address,
+            'postal_code' => $this->postal_code,
+            'state' => $this->state->estado,
+            'city' => $this->city->ciudad,
+            'address' => $this->address,
+            'reference_point' => $this->reference_point
+        ];
+    }
+}
