56f7d9e0a1f241fef25bd0336f4316f2667073e4
Jonlle <jonlle19@gmail.com>
2020-07-29 21:50:33 -0400

    Crea Store y Update Address Requests

---
+++ app/Http/Requests/UpdateAddress.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Requests;
+
+class UpdateAddress extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'address_type_id' => 'required',
+            'postal_code'     => 'required|max:4',
+            'state_id'        => 'required',
+            'city_id'         => 'required',
+            'address'         => 'required|max:200',
+            'reference_point' => 'max:100',
+            'status_id'       => 'max:2'
+        ];
+
+    }
+}
