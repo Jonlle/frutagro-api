ec6dc9468abdcfe0f66ca6c2c8844c6c3476da0b
Jhonatan Llerena <jhllerena@novopayment.com>
2020-08-05 21:47:52 -0400

    Define relación de order_products en Tax

--- app/Tax.php
+++ app/Tax.php
@@ -7,4 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class Tax extends Model
 {
     // protected $primaryKey = 'tax_id';
+    public function order_products()
+    {
+        return $this->hasMany('App\OrderProduct');
+    }
 }
