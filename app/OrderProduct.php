b36d42ce7662fd991b45f716cb1556575ad289ad
Jhonatan Llerena <jhllerena@novopayment.com>
2020-08-05 21:46:05 -0400

    Define relación de tax en pivot OrderProduct

--- app/OrderProduct.php
+++ app/OrderProduct.php
@@ -3,8 +3,14 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\Pivot;
 
 class OrderProduct extends Model
 {
-    // protected $primaryKey = 'order_pro_id';
+    public $timestamps = false;
+
+    public function tax()
+    {
+        return $this->belongsTo('App\Tax');
+    }
 }
