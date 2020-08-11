9214b212933ea3c00f64a2f900512fd2344fdf4f
Jhonatan Llerena <jhllerena@novopayment.com>
2020-08-11 01:01:03 -0400

    Modifica UsersTableSeeder

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -11,38 +11,45 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        $admin_email = new App\UserEmail([
+        $owner_email = new App\UserEmail([
             'email' => 'admin@frutagro.com',
             'principal' => '1'
         ]);
 
-        $admin = App\User::create([
+        $owner = App\User::create([
             'username' => 'admin',
             'role_id' => 'owner',
             'name' => 'Distribuidora Frutagro',
             'password' => Hash::make('frutagro'),
         ]);
 
-        $admin->user_emails()->save($admin_email);
+        $owner->user_emails()->save($owner_email);
 
         // FIXME: Factory users
-        $user_emails = [
+        $admin_emails = [
             ['email' => 'jen_cadiz@hotmail.com', 'principal' => '1'],
-            ['email' => 'jonlle19@gmail.com', 'principal' => '1'],
-            ['email' => 'admin@admin.com', 'principal' => '1'],
+            ['email' => 'jonlle19@gmail.com', 'principal' => '1']
         ];
 
-        $users = [
+        $admins = [
             ['username' => 'CarolGut', 'role_id' => 'admin', 'name' => 'Jennifer Cadiz', 'password' => Hash::make('123')],
-            ['username' => 'Jonlle', 'role_id' => 'admin', 'name' => 'Jhonatan Llerena', 'password' => Hash::make('190792')],
-            ['username' => 'Conejito', 'role_id' => 'person', 'name' => 'Conejito Cadiz', 'password' => Hash::make('123')],
+            ['username' => 'Jonlle', 'role_id' => 'admin', 'name' => 'Jhonatan Llerena', 'password' => Hash::make('190792')]
         ];
 
-        for ($i=0; $i < 3; $i++) {
-            $email_model = new App\UserEmail($user_emails[$i]);
-            $user_model = App\User::create($users[$i]);
-            $user_model->user_emails()->save($email_model);
+        for ($i=0; $i < 2; $i++) {
+            $email_model = new App\UserEmail($admin_emails[$i]);
+            $admin_model = App\User::create($admins[$i]);
+            $admin_model->user_emails()->save($email_model);
         }
+
+        $customer = App\User::create(['username' => 'Conejito', 'role_id' => 'person', 'name' => 'Conejito Cadiz', 'password' => Hash::make('123')]);
+
+        $customer_email = new App\UserEmail(['email' => 'admin@admin.com', 'principal' => '1']);
+
+        $customer_address = new App\UserAddress(['address_type_id' => 'domicilio', 'postal_code' => '1090', 'state_id' => '24', 'city_id' => '149', 'address' => 'El Valle, Caracas']);
+
+        $customer->user_emails()->save($customer_email);
+        $customer->user_addresses()->save($customer_address);
 
         factory(App\User::class,5)->create()->each(function ($user) {
             $user->user_emails()->save(factory(App\UserEmail::class)->make());
