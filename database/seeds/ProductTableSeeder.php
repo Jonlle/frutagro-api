1387e891b0840d10d47da79e8d6ac1eff16b4c72
Jhonatan Llerena <jhllerena@novopayment.com>
2020-08-11 01:05:00 -0400

    Mejora inserción de datos en ProductTableSeeder

--- database/seeds/ProductTableSeeder.php
+++ database/seeds/ProductTableSeeder.php
@@ -21,12 +21,10 @@ class ProductTableSeeder extends Seeder
             ['category_id' => '2', 'sku' => '', 'product_name' => 'Ciruela', 'slug' => '', 'unit_name' => 'Gr', 'unit_cant' => '100', 'price' => 100.00, 'description' => 'Product description', 'currency_code_id' => 'USD']
         ];
 
-        $i = 1;
-        foreach ($products as $row) {
-            $row['slug'] = Str::slug($row['product_name']);
-            $row['sku'] = 'FRU'.strtoupper(substr($row['slug'], 0, 3)).$row['unit_name'][0].'00'.strval($i);
-            App\Product::create($row);
-            $i++;
+        foreach ($products as $key => $value) {
+            $value['slug'] = Str::slug($value['product_name']);
+            $value['sku'] = 'FRU'.strtoupper(substr($value['slug'], 0, 3)).$value['unit_name'][0].'00'.strval($key);
+            App\Product::create($value);
         }
     }
 }
