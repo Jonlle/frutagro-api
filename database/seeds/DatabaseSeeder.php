e7bf311f1b857fd930aa5ef37e5d7d42b816b6e4
Jhonatan Llerena <jhllerena@novopayment.com>
2020-08-12 01:46:19 -0400

    Crea Seeders para Ordenes y Pagos

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,16 +13,22 @@ class DatabaseSeeder extends Seeder
     {
         $this->call(StatusesTableSeeder::class);
         $this->call(DocumentTypesTableSeeder::class);
+        $this->call(AddressTypeTableSeeder::class);
         $this->call(PermissionsTableSeeder::class);
         $this->call(RolesTableSeeder::class);
         $this->call(PermissionRoleTableSeeder::class);
         $this->call(UsersTableSeeder::class);
         $this->call(CategoriesTableSeeder::class);
         $this->call(CurrencyCodesTableSeeder::class);
+        $this->call(TaxesTableSeeder::class);
         $this->call(ProductTableSeeder::class);
-        $this->call(AddressTypeTableSeeder::class);
         $this->call(BannersTableSeeder::class);
         $this->call(FinancialEntitiesTableSeeder::class);
+        $this->call(PaymentTypesTableSeeder::class);
+        $this->call(PaymentsTableSeeder::class);
+        $this->call(DeliveryMethodsTableSeeder::class);
+        $this->call(OrdersTableSeeder::class);
+        $this->call(OrderProductsTableSeeder::class);
         $this->call(EstadosTableSeeder::class);
         $this->call(CiudadesTableSeeder::class);
         $this->call(MunicipiosTableSeeder::class);
---
+++ database/seeds/OrdersTableSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Carbon;
+
+class OrdersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $orders = [
+            ['status_id' => 'co', 'user_id' => 4, 'user_address_id' => 1, 'payment_id' => 1, 'delivery_method_id' => 1, 'grand_total' => 100.00, 'item_count' => 1],
+            ['status_id' => 'pr', 'user_id' => 4, 'user_address_id' => 1, 'payment_id' => 2, 'delivery_method_id' => 1, 'grand_total' => 200.00, 'item_count' => 2],
+            ['status_id' => 'pe', 'user_id' => 4, 'user_address_id' => 1, 'payment_id' => 3, 'delivery_method_id' => 1, 'grand_total' => 300.00, 'item_count' => 3],
+        ];
+
+        foreach ($orders as $row) {
+            $row['order_number'] = substr(Carbon::now()->format('YmdHiu'), 2);
+            App\Order::create($row);
+        }
+    }
+}
