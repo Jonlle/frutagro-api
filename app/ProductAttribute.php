c2078824a1137ba36092790c1fa2ef060d9b4af2
Jhonatan Llerena <jonlle19@gmail.com>
2020-09-27 23:08:35 -0400

    Modifica ProductAttribute Model

--- app/ProductAttribute.php
+++ app/ProductAttribute.php
@@ -6,6 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class ProductAttribute extends Model
 {
+    protected $fillable = [
+        'sku', 'unit_name', 'unit_cant', 'price', 'discount', 'status_id'
+    ];
+
     public function status()
     {
         return $this->belongsTo('App\Status');
