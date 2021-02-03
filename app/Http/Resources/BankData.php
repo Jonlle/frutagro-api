840f15b583574471a16beead5cb0ba8a392e12c5
Jhonatan Llerena <jonlle19@gmail.com>
2021-02-03 00:52:38 -0400

    Crea BankData Resources y Collection

---
+++ app/Http/Resources/BankData.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class BankData extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'payment_type' => $this->payment_type_id,
+            'financial_entity' => new FinancialEntity($this->financialEntity),
+            'supplier_id' => $this->supplier_id,
+            'target_acount' => $this->target_acount,
+            'document_type' => $this->document_type_id,
+            'document' => $this->document,
+            'target_name' => $this->target_name,
+            'file_image' => $this->file_image,
+            'file_path' => $this->file_path,
+            'status' => $this->status_id,
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at
+        ];
+    }
+}
