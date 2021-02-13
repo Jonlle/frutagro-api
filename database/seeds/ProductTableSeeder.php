d5eaa54e6da99ab04cf55502f64a8ed3afe7f4d1
Jhonatan Llerena <jonlle19@gmail.com>
2021-02-12 20:41:52 -0400

    Actualiza ProductTableSeeder

--- database/seeds/ProductTableSeeder.php
+++ database/seeds/ProductTableSeeder.php
@@ -14,12 +14,14 @@ class ProductTableSeeder extends Seeder
     public function run()
     {
         $products = [
-            ['category_id' => '2', 'product_name' => 'Acelga', 'slug' => '', 'description' => 'Product description', 'currency_code_id' => 'USD', "attributes" => [["sku" => "FRUACEK001","unit_name" => "kg","unit_cant" => 10,"price" => 10,"stock" => 10]]],
-            ['category_id' => '2', 'product_name' => 'Aji Dulce', 'slug' => '', 'description' => 'Product description', 'currency_code_id' => 'USD', "attributes" => [["sku" => "FRUAJIK002","unit_name" => "kg","unit_cant" => 10,"price" => 10,"stock" => 10]]],
-            ['category_id' => '2', 'product_name' => 'Aji Picante', 'slug' => '', 'description' => 'Product description', 'currency_code_id' => 'USD', "attributes" => [["sku" => "FRUAJIK003","unit_name" => "kg","unit_cant" => 10,"price" => 10,"stock" => 10]]],
-            ['category_id' => '2', 'product_name' => 'Ajo', 'slug' => '', 'description' => 'Product description', 'currency_code_id' => 'USD', "attributes" => [["sku" => "FRUAJOK004","unit_name" => "kg","unit_cant" => 10,"price" => 10,"stock" => 10]]],
-            ['category_id' => '2', 'product_name' => 'Berenjena', 'slug' => '', 'description' => 'Product description', 'currency_code_id' => 'USD', "attributes" => [["sku" => "FRUBERK005","unit_name" => "kg","unit_cant" => 10,"price" => 10,"stock" => 10]]],
-            ['category_id' => '2', 'product_name' => 'Ciruela', 'slug' => '', 'description' => 'Product description', 'currency_code_id' => 'USD', "attributes" => [["sku" => "FRUCIRK006","unit_name" => "kg","unit_cant" => 10,"price" => 10,"stock" => 10],["sku" => "FRUCIRC007","unit_name" => "ce","unit_cant" => 10,"price" => 20,"stock" => 10]]]
+            ['category_id' => '2', 'product_name' => 'Acelga', 'slug' => '', 'description' => 'Product description', 'currency_code_id' => 'USD', "attributes" => [["sku" => "VEGACEK001","unit_name" => "kg","unit_cant" => 10,"price" => 10,"stock" => 10]]],
+            ['category_id' => '2', 'product_name' => 'Aji Dulce', 'slug' => '', 'description' => 'Product description', 'currency_code_id' => 'USD', "attributes" => [["sku" => "VEGAJIK002","unit_name" => "kg","unit_cant" => 10,"price" => 10,"stock" => 10]]],
+            ['category_id' => '2', 'product_name' => 'Aji Picante', 'slug' => '', 'description' => 'Product description', 'currency_code_id' => 'USD', "attributes" => [["sku" => "VEGAJIK003","unit_name" => "kg","unit_cant" => 10,"price" => 10,"stock" => 10]]],
+            ['category_id' => '2', 'product_name' => 'Ajo', 'slug' => '', 'description' => 'Product description', 'currency_code_id' => 'USD', "attributes" => [["sku" => "VEGAJOK004","unit_name" => "kg","unit_cant" => 10,"price" => 10,"stock" => 10]]],
+            ['category_id' => '2', 'product_name' => 'Berenjena', 'slug' => '', 'description' => 'Product description', 'currency_code_id' => 'USD', "attributes" => [["sku" => "VEGBERK005","unit_name" => "kg","unit_cant" => 10,"price" => 10,"stock" => 10]]],
+            ['category_id' => '2', 'product_name' => 'Ciruela', 'slug' => '', 'description' => 'Product description', 'currency_code_id' => 'USD', "attributes" => [["sku" => "VEGCIRK006","unit_name" => "kg","unit_cant" => 10,"price" => 10,"stock" => 10],["sku" => "VEGCIRC007","unit_name" => "ce","unit_cant" => 10,"price" => 20,"stock" => 10]]],
+            ['category_id' => '1', 'product_name' => 'Fresa', 'slug' => '', 'description' => 'Product description', 'currency_code_id' => 'USD', "attributes" => [["sku" => "FRUFREK007","unit_name" => "kg","unit_cant" => 10,"price" => 10,"stock" => 10]]],
+            ['category_id' => '3', 'product_name' => 'Cebolla', 'slug' => '', 'description' => 'Product description', 'currency_code_id' => 'USD', "attributes" => [["sku" => "VERCEBK008","unit_name" => "kg","unit_cant" => 10,"price" => 10,"stock" => 10]]]
         ];
 
         foreach ($products as $key => $value) {
