08a671179312523d67b1e112f076bfdfef25ece2
Jhonatan Llerena <jonlle19@gmail.com>
2021-02-16 18:19:41 -0400

    Crea RecommendedProduct Request

---
+++ app/Http/Requests/RecommendedProduct.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Requests;
+
+
+class RecommendedProduct extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'id' => 'required',
+            'category_id' => 'required',
+            'take' => 'nullable'
+        ];
+    }
+}
