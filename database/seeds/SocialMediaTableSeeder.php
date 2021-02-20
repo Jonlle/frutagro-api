797fe560b31b143def7f3cc76ca20e68dae6c040
Jhonatan Llerena <jonlle19@gmail.com>
2021-02-20 00:19:24 -0400

    Inserta social media instagram en Seeder

--- database/seeds/SocialMediaTableSeeder.php
+++ database/seeds/SocialMediaTableSeeder.php
@@ -12,7 +12,7 @@ class SocialMediaTableSeeder extends Seeder
     public function run()
     {
         $social_media = [
-            ['icon_name' => 'fab fa-instagram', 'icon_size' => 'fa-3x', 'name' => 'instagram'],
+            ['icon_name' => 'fab fa-instagram', 'icon_size' => 'fa-3x', 'name' => 'instagram', 'url' => 'https://www.instagram.com/distribuidorafrutagro/', 'status_id' => 'en'],
             ['icon_name' => 'fab fa-facebook-f', 'icon_size' => 'fa-2x', 'name' => 'facebook'],
             ['icon_name' => 'fab fa-whatsapp', 'icon_size' => 'fa-2x', 'name' => 'whatsapp'],
             ['icon_name' => 'fab fa-telegram-plane', 'icon_size' => 'fa-3x', 'name' => 'telegram'],
