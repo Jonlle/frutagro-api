bb41ccda225695b4259e9789b5afe77842c9ee2f
Jhonatan Llerena <jonlle19@gmail.com>
2021-02-25 00:12:48 -0400

    Agrega monedas a CurrencyCodes Seeder

--- database/seeds/CurrencyCodesTableSeeder.php
+++ database/seeds/CurrencyCodesTableSeeder.php
@@ -13,7 +13,9 @@ class CurrencyCodesTableSeeder extends Seeder
     {
         $currency_codes = [
             ['id' => 'VES', 'currency_name' => 'Bolívar Soberano', 'currency_symbol' => 'Bs.S', 'exchange_rate' => 1950000],
-            ['id' => 'USD', 'currency_name' => 'United States Dollar', 'currency_symbol' => '$', 'exchange_rate' => 1, 'default' => '1'],
+            ['id' => 'USD', 'currency_name' => 'Dólar estadounidense', 'currency_symbol' => '$', 'exchange_rate' => 1, 'default' => '1'],
+            ['id' => 'EUR', 'currency_name' => 'Euro', 'currency_symbol' => '€', 'exchange_rate' => 0.82, 'status_id' => 'di'],
+            ['id' => 'COP', 'currency_name' => 'Peso colombiano', 'currency_symbol' => '$', 'exchange_rate' => 3565, 'status_id' => 'di'],
         ];
 
         foreach ($currency_codes as $row) {
