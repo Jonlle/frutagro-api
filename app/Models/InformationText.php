0b843a5c37658006c66fabc723b1de6c118d48ea
Jhonatan Llerena <jonlle19@gmail.com>
2021-03-12 19:38:03 -0400

    Crea scopes para querys en InformationText

--- app/Models/InformationText.php
+++ app/Models/InformationText.php
@@ -26,6 +26,28 @@ class InformationText extends Model
         'status_id', 'section_name'
     ];
 
+    /**
+     * Scope a query to include only informational texts from the login, register and recover password sections.
+     *
+     * @param  \Illuminate\Database\Eloquent\Builder  $query
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function scopeAuthSections($query)
+    {
+        return $query->whereIn('section_name', ['login', 'register', 'recover_password']);
+    }
+
+    /**
+     * Scope a query to include only informational texts from the mission and vision sections.
+     *
+     * @param  \Illuminate\Database\Eloquent\Builder  $query
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function scopeMissionVision($query)
+    {
+        return $query->whereIn('section_name', ['mission', 'vision']);
+    }
+
     public function tags()
     {
         return $this->belongsToMany('App\Models\Tag');
