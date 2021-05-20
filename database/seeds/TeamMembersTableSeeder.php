25967040d1e1689c04632cbdbd46cf0a6369f142
Jhonatan Llerena <jonlle19@gmail.com>
2021-05-20 16:37:03 -0400

    Moficia file_image en TeamMembersTableSeeder

--- database/seeds/TeamMembersTableSeeder.php
+++ database/seeds/TeamMembersTableSeeder.php
@@ -12,9 +12,9 @@ class TeamMembersTableSeeder extends Seeder
     public function run()
     {
         $teamMembers = [
-            ['name' => 'Stave Martin', 'position' => 'Manager', 'description' => 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been.', 'file_image' => '10_team.jpg'],
-            ['name' => 'Mark Smith', 'position' => 'Designer', 'description' => 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been.', 'file_image' => '98_team.jpg'],
-            ['name' => 'Ryan Printz', 'position' => 'Marketing', 'description' => 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been.', 'file_image' => '50_team.jpg'],
+            ['name' => 'Stave Martin', 'position' => 'Manager', 'description' => 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been.', 'file_image' => '10_team.png'],
+            ['name' => 'Mark Smith', 'position' => 'Designer', 'description' => 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been.', 'file_image' => '98_team.png'],
+            ['name' => 'Ryan Printz', 'position' => 'Marketing', 'description' => 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been.', 'file_image' => '50_team.png'],
         ];
 
         foreach ($teamMembers as $row) {
