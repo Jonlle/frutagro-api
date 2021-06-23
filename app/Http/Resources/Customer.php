d1b1e71afebaa913b9489758ca48f4a1bc30de81
Jhonatan Llerena <jonlle19@gmail.com>
2021-06-23 01:48:08 -0400

    Crea métodos para ocultar keys en CustomerResource

--- app/Http/Resources/Customer.php
+++ app/Http/Resources/Customer.php
@@ -7,14 +7,19 @@ use Illuminate\Http\Resources\Json\JsonResource;
 class Customer extends JsonResource
 {
     /**
+     * @var array
+     */
+    protected $withoutFields = [];
+
+    /**
      * Transform the resource into an array.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param  \Illuminate\Http\Request
      * @return array
      */
     public function toArray($request)
     {
-        return [
+        return $this->filterFields([
             'id' => $this->id,
             'username' => $this->username,
             'role' => $this->role->name,
@@ -29,6 +34,27 @@ class Customer extends JsonResource
             'email_verified_at' => $this->email_verified_at,
             'created_at' => $this->created_at,
             'updated_at' => $this->updated_at,
-        ];
+        ]);
+    }
+    /**
+     * Set the keys that are supposed to be filtered out.
+     *
+     * @param array $fields
+     * @return $this
+     */
+    public function hide(array $fields)
+    {
+        $this->withoutFields = $fields;
+        return $this;
+    }
+    /**
+     * Remove the filtered keys.
+     *
+     * @param $array
+     * @return array
+     */
+    protected function filterFields($array)
+    {
+        return collect($array)->forget($this->withoutFields)->toArray();
     }
 }
